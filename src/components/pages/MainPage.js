02a300b7fbf562fc99848b989c16b2e7f5317435
CuongGustav <cuonggustav@gmail.com>
2025-02-11 21:10:32 +0700

    Done Chapter Section

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../global/Header';
 import Hero from '../hero';
 import Achieve from '../achieve';
+import Chapter from '../chapter';
 
 const MainPage = ({ header }) => {
     return(
@@ -9,6 +10,7 @@ const MainPage = ({ header }) => {
             <Header header={header}/>
             <Hero isBg='yes'/>
             <Achieve isBg=''/>
+            <Chapter isBg='yes'/>
         </>
     )
 }
